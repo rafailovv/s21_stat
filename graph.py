865f2029bf74806e70cc7fcb849fe58dbd0e95fc
Arsen Rafailov <arsraf00@gmail.com>
2025-02-24 00:29:00 +0300

    Add first stable version

---
+++ graph.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+import matplotlib
+from matplotlib import pyplot as plt
+from datetime import datetime
+
+matplotlib.use('SVG')
+ABSOLUTE_FULL_PATH = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')
+ABSOLUTE_PATH = ABSOLUTE_FULL_PATH[:ABSOLUTE_FULL_PATH.rindex('/')]
+
+def createDatetimes() -> list[datetime]:
+    dates = []
+    for month in range(1, 3):
+        if month == 1:
+            for day in range(23, 32):
+                dates.append(datetime(2025, month, day))
+        else:
+            for day in range(1, 15):
+                dates.append(datetime(2025, month, day))
+    return dates
+
+
+def getHoursByNickname(df: pd.DataFrame, nick: str) -> pd.Series:
+    return df[df.nickname == nick].iloc[0, 30:] / 60
+
+def getExamsByNickname(df: pd.DataFrame, nick: str) -> pd.Series:
+    return df[df.nickname == nick].iloc[0, [12, 18, 24, 29]]
+
+
+def formatDates(dates: list[datetime], format: str) -> list[str]:
+    return list(map(lambda x: x.strftime(format), dates))
+
+
+def getTimeDynamic(df: pd.DataFrame, nicknames: list[str], dates: list[datetime]) -> None:
+    plt.figure(figsize=(20, 10))
+    plt.title("Время в кампусе")
+    plt.xlabel("Дата")
+    plt.ylabel("Время, часы")
+    plt.yticks(list(range(0, 25)))
+    plt.grid(alpha=0.5, linestyle = "--", linewidth = 0.5)
+
+    for nick in nicknames:
+        if nick in df.nickname.to_list():
+            plt.plot(dates, getHoursByNickname(df, nick), label=nick)
+
+    plt.legend()
+    plt.savefig(f"{ABSOLUTE_PATH}/images/time.png", bbox_inches="tight")
+    plt.clf()
+
+
+def getExamDynamic(df: pd.DataFrame, nicknames: list[str], exams: list[str]) -> None:
+    plt.figure(figsize=(15, 10))
+    plt.title("Результаты экзаменов")
+    plt.xlabel("Экзамен")
+    plt.ylabel("XP за экзамен")
+    plt.yticks(list(range(0, 46)))
+    plt.grid(alpha=0.5, linestyle = "--", linewidth = 0.5)
+        
+    plt.plot(exams, [45] * 4, label = "Максимум", linestyle="--", color="red")
+
+    for nick in nicknames:
+        if nick in df.nickname.to_list():
+            plt.plot(exams, getExamsByNickname(df, nick), label=nick)
+
+    plt.legend()
+    plt.savefig(f"{ABSOLUTE_PATH}/images/exam.png", bbox_inches="tight")
+    plt.clf()
---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import telebot
+import pandas as pd
+import functions.graph as graph
+import dotenv
+
+dotenv.load_dotenv()
+
+API_TOKEN = os.getenv("API_TOKEN")
+
+bot = telebot.TeleBot(API_TOKEN, parse_mode="HTML")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "<b>Бот запущен!</b>")
+    
+    
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 "Для получения статистики - введи ники через запятую\n<i>*для получения средних значений, введи в качестве ника Средние</i>")
+
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def echo_message(message):
+    df = pd.read_csv(f"{graph.ABSOLUTE_PATH}/data/users_means.csv")
+
+    dates = graph.formatDates(graph.createDatetimes(), "%d.%m")
+    exams = ["E01D05", "E02D12", "E03D19", "E04D26"]
+
+    nicknames = list(map(lambda x: x.strip(), message.text.split(',')))
+    founded_nicknames_count = sum(df.nickname.isin(nicknames))
+    
+    if founded_nicknames_count > 0:
+        graph.getExamDynamic(df, nicknames, exams)
+        graph.getTimeDynamic(df, nicknames, dates)
+        
+        exam_img = telebot.types.InputMediaPhoto(telebot.types.InputFile(f"{graph.ABSOLUTE_PATH}/images/exam.png"), caption="Экзамен и время в кампусе", show_caption_above_media=True)
+        time_img = telebot.types.InputMediaPhoto(telebot.types.InputFile(f"{graph.ABSOLUTE_PATH}/images/time.png"), show_caption_above_media=True)
+        
+        bot.send_media_group(message.chat.id,
+                            [exam_img, time_img],
+                            reply_to_message_id=message.id)
+    else:
+        bot.reply_to(message,
+                     "<b>Ни одного ника в сообщении не найдено!</b>")
+
+bot.infinity_polling()
