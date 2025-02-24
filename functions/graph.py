b8be4a0e35a74f9890c65e88d26a9b13de080d9d
Arsen Rafailov <arsraf00@gmail.com>
2025-02-24 16:22:22 +0300

    format code by pylint

--- functions/graph.py
+++ functions/graph.py
@@ -1,26 +1,32 @@
+""" Module providing graph for statistics """
+
 import os
 from io import BytesIO
+from datetime import datetime
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
-from datetime import datetime
 
 matplotlib.use('SVG')
 PATH = os.path.dirname(os.path.abspath(os.path.join(__file__, "..")))
 
-def getHoursByNickname(df: pd.DataFrame, nick: str) -> pd.Series:
+def get_hours_by_nickname(df: pd.DataFrame, nick: str) -> pd.Series:
+    """ Return list of hours in campus by days by nickname """
     return df[df.nickname == nick].iloc[0, 30:] / 60
 
 
-def getExamsByNickname(df: pd.DataFrame, nick: str) -> pd.Series:
+def get_exams_by_nickname(df: pd.DataFrame, nick: str) -> pd.Series:
+    """ Return list of exams xp by nickname """
     return df[df.nickname == nick].iloc[0, [12, 18, 24, 29]]
 
 
-def formatDates(dates: list[datetime], format: str) -> list[str]:
-    return list(map(lambda x: x.strftime(format), dates))
+def format_dates(dates: list[datetime], date_format: str) -> list[str]:
+    """ Return list of dates formatting them in date_format """
+    return list(map(lambda x: x.strftime(format=date_format), dates))
 
 
-def createDatetimes() -> list[datetime]:
+def create_datetimes() -> list[datetime]:
+    """ Creating list of datetime objects from 23.01.2025 to 14.02.2025 """
     dates = []
     for month in range(1, 3):
         if month == 1:
@@ -32,20 +38,21 @@ def createDatetimes() -> list[datetime]:
     return dates
 
 
-def getExamDynamic(df: pd.DataFrame, nicknames: list[str], exams: list[str]) -> bytes:
+def get_exam_dynamic(df: pd.DataFrame, nicknames: list[str], exams: list[str]) -> bytes:
+    """ Returns bytes object with exams graphs for nicknames picture (png) """
     plt.figure(figsize=(10, 10))
     plt.title("Результаты экзаменов")
     plt.xlabel("Экзамен")
     plt.ylabel("XP за экзамен")
     plt.yticks(list(range(0, 46)))
     plt.grid(alpha=0.5, linestyle = "--", linewidth = 0.5)
-        
+
     plt.plot(exams, [45] * 4, label = "Максимум", linestyle="--", color="red")
 
     for nick in nicknames:
         if nick in df.nickname.to_list():
-            plt.plot(exams, getExamsByNickname(df, nick), label=nick)
-            
+            plt.plot(exams, get_exams_by_nickname(df, nick), label=nick)
+
     plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
     plt.tight_layout()
 
@@ -53,9 +60,10 @@ def getExamDynamic(df: pd.DataFrame, nicknames: list[str], exams: list[str]) -> 
     plt.savefig(buffer, format="png")
     plt.clf()
     return buffer.getvalue()
-    
-    
-def getTimeDynamic(df: pd.DataFrame, nicknames: list[str], dates: list[datetime]) -> bytes:
+
+
+def get_time_dynamic(df: pd.DataFrame, nicknames: list[str], dates: list[datetime]) -> bytes:
+    """ Returns bytes object with time in campus by days graphs for nicknames picture (png) """
     plt.figure(figsize=(20, 10))
     plt.title("Время в кампусе")
     plt.xlabel("Дата")
@@ -65,7 +73,7 @@ def getTimeDynamic(df: pd.DataFrame, nicknames: list[str], dates: list[datetime]
 
     for nick in nicknames:
         if nick in df.nickname.to_list():
-            plt.plot(dates, getHoursByNickname(df, nick), label=nick)
+            plt.plot(dates, get_hours_by_nickname(df, nick), label=nick)
 
     plt.legend(bbox_to_anchor=(1.07, 1), borderaxespad=0)
     plt.tight_layout()
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+""" Bot entry point """
+
 import os
 import io
 import telebot
 import pandas as pd
-import functions.graph as graph
 import dotenv
+import functions.graph as graph
 
 dotenv.load_dotenv()
 
@@ -12,38 +14,42 @@ API_TOKEN = os.getenv("API_TOKEN")
 bot = telebot.TeleBot(API_TOKEN, parse_mode="HTML")
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def send_welcome(message: telebot.types.Message) -> None:
+    """ Send welcome message by start command """
     bot.reply_to(message, "<b>Бот запущен!</b>")
-    
-    
+
+
 @bot.message_handler(commands=['help'])
-def send_welcome(message):
+def send_help(message: telebot.types.Message) -> None:
+    """ Send help message for help command """
     bot.reply_to(message,
                  ("Для получения статистики - введи ники через запятую\n"
-                 "При попытке отобразить <b>более 45 ников</b>, в легенде будут указаны <b>только</b> первые 45 ников\n"
+                 "При попытке отобразить <b>более 45 ников</b>, "
+                 "в легенде будут указаны <b>только</b> первые 45 ников\n"
                  "<i>*для получения средних значений, введите в качестве ника Средние</i>"))
 
 
 @bot.message_handler(func=lambda message: True, content_types=["text"])
-def echo_message(message):
+def send_stats(message: telebot.types.Message) -> None:
+    """ Send graphs by nicknames in message """
     df = pd.read_csv(os.path.join(graph.PATH, "data\\users_means.csv"))
 
-    dates = graph.formatDates(graph.createDatetimes(), "%d.%m")
+    dates = graph.format_dates(graph.create_datetimes(), "%d.%m")
     exams = ["E01D05", "E02D12", "E03D19", "E04D26"]
 
     nicknames = sorted(set(map(lambda x: x.strip(), message.text.split(','))))
     founded_nicknames_count = sum(df.nickname.isin(nicknames))
-    
+
     if founded_nicknames_count > 0:
-        exam_img_buffer = io.BytesIO(graph.getExamDynamic(df, nicknames, exams))
-        time_img_buffer = io.BytesIO(graph.getTimeDynamic(df, nicknames, dates))
-        
+        exam_img_buffer = io.BytesIO(graph.get_exam_dynamic(df, nicknames, exams))
+        time_img_buffer = io.BytesIO(graph.get_time_dynamic(df, nicknames, dates))
+
         exam_img = telebot.types.InputMediaPhoto(telebot.types.InputFile(exam_img_buffer),
                                                  caption="Экзамен и время в кампусе",
                                                  show_caption_above_media=True)
         time_img = telebot.types.InputMediaPhoto(telebot.types.InputFile(time_img_buffer),
                                                  show_caption_above_media=True)
-        
+
         bot.send_media_group(message.chat.id,
                             [exam_img, time_img],
                             reply_to_message_id=message.id)
@@ -52,4 +58,5 @@ def echo_message(message):
                      "<b>Ни одного ника в сообщении не найдено!</b>")
 
 
-bot.infinity_polling()
+if __name__ == "__main__":
+    bot.infinity_polling()
